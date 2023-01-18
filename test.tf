05e73da29fb9fe75be09e354f503bbbd9fd7dff3
Nelson Florez <nelsonflorez11@gmail.com>
2023-01-18 14:19:55 -0500

    test:02

--- test.tf
+++ test.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "esto_es_test"
+    Environment = "test"
   }
 }
